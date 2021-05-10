41e12c1530aabab213be389f76e3368d5aa431fc
KevinMokoro <klosvoke1@gmail.com>
2021-05-10 18:09:22 +0300

    Modify app.java given routes.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -132,6 +132,21 @@ public class App {
             }
         });
 
+        exception(ApiException.class, (exception, req, res) -> {
+            ApiException err = exception;
+            Map<String, Object> jsonMap = new HashMap<>();
+            jsonMap.put("status", err.getStatusCode());
+            jsonMap.put("errorMessage", err.getMessage());
+            res.type("application/json");
+            res.status(err.getStatusCode());
+            res.body(gson.toJson(jsonMap));
+        });
+
+
+        after((req, res) ->{
+            res.type("application/json");
+        });
+
 
 
 
